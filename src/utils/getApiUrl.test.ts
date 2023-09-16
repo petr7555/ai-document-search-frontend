743019df5b6f45e957e570ead0e7f7d1cb91cbc5
Petr Janik <peta.janik@email.cz>
2023-09-16 23:10:15 +0200

    Fix tests

--- src/utils/getApiUrl.test.ts
+++ src/utils/getApiUrl.test.ts
@@ -1,20 +1,24 @@
 import getApiUrl from './getApiUrl';
 
+const realLocation = window.location;
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+
 it('returns the correct API URL for development', () => {
-  Object.defineProperty(window, 'location', {
-    value: {
-      hostname: 'localhost'
-    }
-  });
+  jest.spyOn(window, 'location', 'get').mockImplementation(() => ({
+    ...realLocation,
+    hostname: 'localhost'
+  }));
   expect(getApiUrl()).toEqual('http://localhost:8000');
 });
 
 it('returns the correct API URL for production', () => {
-  Object.defineProperty(window, 'location', {
-    value: {
-      hostname: 'not-localhost'
-    }
-  });
+  jest.spyOn(window, 'location', 'get').mockImplementation(() => ({
+    ...realLocation,
+    hostname: 'not-localhost'
+  }));
   expect(getApiUrl()).toEqual(
     'https://ai-document-search-backend.azurewebsites.net'
   );
