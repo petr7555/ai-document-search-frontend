8482af210f80e551ebe9e2e148268544106280fa
mariubg <mariubg@stud.ntnu.no>
2023-09-20 13:24:22 +0200

    Send form-data to api

--- src/utils/authenticateUser.ts
+++ src/utils/authenticateUser.ts
@@ -1,15 +1,16 @@
 import axios from 'axios';
 
-const API_URL_PROD = 'https://api.example.com';
+const API_URL_PROD = 'http://127.0.0.1:8000/auth';
 
 export const authenticateUser = async (
   username: string,
   password: string
 ): Promise<string> => {
-  const response = await axios.post(`${API_URL_PROD}/token`, {
-    username,
-    password
-  });
+
+  const form = new FormData();
+  form.append('username', username);
+  form.append('password', password)
+  const response = await axios.post(`${API_URL_PROD}/token`, form);
 
   //check if the response is valid
   if (response.status !== 200) {
