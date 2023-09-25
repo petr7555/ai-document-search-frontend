8070d214dbc5936389df6a8a765c91259957d0f3
unknown <robingg@stud.ntnu.no>
2023-09-25 14:09:38 +0200

    Added e2e test for current login page functionality. Added component test for current navbar and primarybutton components.

--- cypress/component/BackendHealthCheck.cy.tsx
+++
@@ -1,6 +0,0 @@
-describe('BackendHealthCheck.cy.tsx', () => {
-  it('playground', () => {
-    // cy.mount()
-    //This is an example test, there is nothing to test for backend health check
-  })
-})
---
+++ cypress/component/PrimaryButton.cy.tsx
@@ -0,0 +1,10 @@
+import { PrimaryButton } from '../../src/components/Button/PrimaryButton'
+
+describe('Navbar component', () => {
+    it('Navbar component test', () => {
+      cy.mount(<PrimaryButton> Test Text </PrimaryButton>)
+      
+      cy.get('button').should('contain.text', "Test Text")
+      cy.get('button').click()
+    })
+  })
